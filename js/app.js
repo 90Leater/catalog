71ec3bb80d029c0ff73fa914218c78cddf2d3780
90Leater <90.leather@gmail.com>
2026-06-20 14:29:10 +0700

    Fetch and display products from JSON file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
-fetch("product.json")
+fetch('product.json')
 .then(res => res.json())
-.then(data => {
-    console.log(data);
+.then(products => {
+
+    const container = document.getElementById('products');
+
+    products.forEach(product => {
+
+        const imagePath =
+        `Images/${product.folder}/${product.warna}`;
+
+        const card = `
+        <div class="card">
+            <img src="${imagePath}" alt="${product.nama}">
+            <div class="card-body">
+                <h3>${product.nama}</h3>
+                <p>${product.kategori}</p>
+
+                <a class="btn"
+                href="https://wa.me/6281234567890?text=Saya ingin pesan ${product.nama}"
+                target="_blank">
+                Tanya Harga
+                </a>
+            </div>
+        </div>
+        `;
+
+        container.innerHTML += card;
+
+    });
+
 });
