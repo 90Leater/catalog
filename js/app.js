a14b8b173f4a458944308f23e5b1bde268f5e431
90Leater <90.leather@gmail.com>
2026-06-22 16:00:26 +0700

    Add back-to-top button functionality

--- js/app.js
+++ js/app.js
@@ -386,6 +386,38 @@ document
     renderPagination(products);
 
 });
+window.addEventListener('scroll', () => {
+
+    const btn =
+    document.getElementById('backToTop');
+
+    if(window.scrollY > 300){
+
+        btn.style.display =
+        'block';
+
+    }else{
+
+        btn.style.display =
+        'none';
+
+    }
+
+});
+
+document
+.getElementById('backToTop')
+.addEventListener('click', () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:'smooth'
+
+    });
+
+});
 
 document.addEventListener('click', function(e){
 
