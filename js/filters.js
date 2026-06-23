7e7ece3a72da22942d40878b2fca854a907306bd
90Leater <90.leather@gmail.com>
2026-06-23 15:02:49 +0700

    Add category loading and product filtering functions
    Implemented category loading and product filtering based on keyword and category selection.

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,86 @@
-function loadCategories(){}
+function loadCategories(){
 
-function filterProducts(){}
+    const categories =
+    [...new Set(
+        products.map(
+            product => product.kategori
+        )
+    )];
+
+    kategoriFilter.innerHTML =
+    '<option value="">Semua Kategori</option>';
+
+    categories.forEach(category => {
+
+        const option =
+        document.createElement('option');
+
+        option.value =
+        category;
+
+        option.textContent =
+        category;
+
+        kategoriFilter.appendChild(option);
+
+    });
+
+}
+
+function filterProducts(){
+
+    const keyword =
+    searchInput.value.toLowerCase();
+
+    const kategori =
+    kategoriFilter.value;
+
+    const filtered =
+    products.filter(product => {
+
+        const cocokKeyword =
+
+            product.nama
+            .toLowerCase()
+            .includes(keyword)
+
+            ||
+
+            product.kode
+            .toLowerCase()
+            .includes(keyword);
+
+        const cocokKategori =
+
+            kategori === ''
+
+            ||
+
+            product.kategori === kategori;
+        const favorites =
+        getFavorites();
+
+        const cocokFavorit =
+
+            currentView === 'all'
+
+            ||
+
+            favorites.includes(
+                product.id
+            );
+
+        return cocokKeyword &&
+               cocokKategori &&
+               cocokFavorit;
+
+    });
+
+    currentPage = 1;
+    if(sortFilter.value === 'az'){
+
+    filtered.sort((a,b) =>
+        a.nama.localeCompare(b.nama)
+    );
+
+}
