9a6ccbd2674a9b827f558444f5a9318e32c13ce2
90Leater <90.leather@gmail.com>
2026-06-23 16:29:05 +0700

    Initialize product management variables and fetch data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+let products = [];
+
+let currentPage = 1;
+let currentView = 'all';
+let currentProductIndex = 0;
+let selectedColor = '';
+
+let selectedProducts = [];
+
+const productsPerPage = 10;
+
+const container = document.getElementById('products');
+
+const pagination = document.getElementById('pagination');
+
+const productCount = document.getElementById('productCount');
+
+const searchInput = document.getElementById('search');
+
+const kategoriFilter = document.getElementById('kategoriFilter');
+
+const sortFilter = document.getElementById('sortFilter');
+
 fetch('product.json')
 .then(res => res.json())
 .then(data => {
