c00f9f6a95731475bca7ffc7edb194775fc3e2d3
90Leater <90.leather@gmail.com>
2026-06-23 15:24:49 +0700

    Fix service worker registration syntax

--- js/pwa.js
+++ js/pwa.js
@@ -1,6 +1,13 @@
-navigator.serviceWorker
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker
     .register('./sw.js')
     .then(() => {
-        console.log('Service Worker Registered');
+
+        console.log(
+            'Service Worker Registered'
+        );
+
+    });
 
 }
