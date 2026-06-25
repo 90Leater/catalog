02c9906193b5403cdc36012d176db8e6d6c6c4fd
90Leater <90.leather@gmail.com>
2026-06-25 15:58:49 +0700

    Replace loadProducts with listenProducts

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,11 +1,5 @@
 import {
-    db,
-    collection,
-    getDocs
-} from "../../firebase-admin.js";
-
-import {
-    loadProducts
+    listenProducts
 } from "./products.js";
 
 import {
@@ -13,7 +7,6 @@ import {
 } from "./import.js";
 
 import "./modal.js";
-
 import "./create-product.js";
 
 document.addEventListener(
@@ -29,116 +22,6 @@ function init(){
 
     setupImportButton();
 
-    loadProducts();
-
-}
-
-const productTable =
-document.getElementById(
-    'productTable'
-);
-
-const totalProducts =
-document.getElementById(
-    'totalProducts'
-);
-
-document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-        loadProducts();
-    }
-);
-
-async function loadProducts(){
-
-    try{
-
-        productTable.replaceChildren();
-
-        const loadingRow =
-        document.createElement('tr');
-
-        loadingRow.innerHTML = `
-            <td colspan="5">
-                Memuat data...
-            </td>
-        `;
-
-        productTable.appendChild(
-            loadingRow
-        );
-
-        const snapshot =
-        await getDocs(
-            collection(
-                db,
-                'products'
-            )
-        );
-
-        productTable.replaceChildren();
-
-        let total = 0;
-
-        snapshot.forEach(doc=>{
-
-            total++;
-
-            createProductRow(
-                doc.data()
-            );
-
-        });
-
-        totalProducts.textContent =
-        total;
-
-    }catch(error){
-
-        console.error(error);
-
-        productTable.innerHTML = `
-            <tr>
-                <td colspan="5">
-                    Gagal mengambil data.
-                </td>
-            </tr>
-        `;
-
-    }
-
-}
-
-function createProductRow(product){
-
-    const row =
-    document.createElement('tr');
-
-    row.innerHTML = `
-        <td>${product.id}</td>
-
-        <td>${product.nama}</td>
-
-        <td>${product.kategori}</td>
-
-        <td>
-            ${product.active ? 'Aktif' : 'Nonaktif'}
-        </td>
-
-        <td>
-
-            <button class="editBtn">
-                Edit
-            </button>
-
-            <button class="deleteBtn">
-                Hapus
-            </button>
-
-        </td>
-    `;
-
-    productTable.appendChild(row);
+    listenProducts();
 
 }
