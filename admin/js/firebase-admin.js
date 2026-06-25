4bfeb90fdd993fb81ffcd51e7ddb2b1ced8a21c9
90Leater <90.leather@gmail.com>
2026-06-25 14:15:19 +0700

    Initialize Firebase and export Firestore/Storage functions
    This file initializes Firebase with Firestore and Storage services, exporting necessary functions for database operations.

---
+++ admin/js/firebase-admin.js
@@ -0,0 +1,71 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    getDocs,
+    collection,
+    query,
+    where,
+    orderBy,
+    limit,
+    setDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    increment,
+    serverTimestamp,
+    writeBatch
+}
+    from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+    import {
+        getStorage,
+        ref,
+        uploadBytes,
+        getDownloadURL,
+        deleteObject
+}
+    from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
+
+    const firebaseConfig = {
+    apiKey: "AIzaSyDwpejFLs4uyMcd2-3_J4wq55NVfaas5ig",
+    authDomain: "go-leather-ind.firebaseapp.com",
+    projectId: "go-leather-ind",
+    storageBucket: "go-leather-ind.firebasestorage.app",
+    messagingSenderId: "771828187046",
+    appId: "1:771828187046:web:46cff406fb2e5089c71967",
+    measurementId: "G-TR754VP1QN"
+  };
+
+const app =
+initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
+
+export {
+    doc,
+    getDoc,
+    getDocs,
+    collection,
+    query,
+    where,
+    orderBy,
+    limit,
+    setDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    increment,
+    serverTimestamp,
+    writeBatch,
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
+};
