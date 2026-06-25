aa09b1caf1010135a7f7449c9b35caa7fdbf72cb
90Leater <90.leather@gmail.com>
2026-06-25 11:52:30 +0700

    Add alerts for future product features

---
+++ admin/admin.js
@@ -0,0 +1,23 @@
+document
+.getElementById(
+'addProduct'
+)
+.onclick=()=>{
+
+alert(
+'Fitur tambah produk akan dibuat berikutnya.'
+);
+
+};
+
+document
+.getElementById(
+'importProduct'
+)
+.onclick=()=>{
+
+alert(
+'Fitur import product.json akan dibuat berikutnya.'
+);
+
+};
